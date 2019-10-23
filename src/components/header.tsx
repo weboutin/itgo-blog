e2d0c9daf0a49d46da26df6d7b161a1fa2e5f98f
itgo <634872896@qq.com>
2019-10-23 17:51:26 +0800

    feat: init

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import githublogo from '../github.png';
+import './header.css';
+
+
+const Header: React.FC = () => (
+  <></>
+);
+
+export default Header;
+
+// export default class Header extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <div className="header">
+//           <a className="logo" href="/">Itgo</a>
+//           <a className="description" href="https://github.com/weboutin">
+//             <img src={githublogo} className="github-icon" alt="git" />
+//           </a>
+//         </div>
+//         <div className="headerEmpty" />
+//       </>
+//     );
+//   }
+// }
